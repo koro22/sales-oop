330627fb60c9b6279f9cb9c56f8e15b6b8dd3d0a
haruka <yaot739@gmail.com>
2024-11-22 09:39:49 +0900

    Initial commit

--- classes/Database.php
+++ classes/Database.php
@@ -3,7 +3,7 @@
     class Database {
         private $server_name = "localhost";
         private $db_username = "root";
-        private $db_password = "";
+        private $db_password = "root";
         private $db_name = "sales_oop";
         protected $conn;
 
